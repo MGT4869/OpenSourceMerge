de4356ce33c45919469de441277e5e392965cc43
MGT4869 <47241183+MGT4869@users.noreply.github.com>
2022-05-25 01:02:17 +0900

    GUI구현

--- STTAndTranslation.py
+++ STTAndTranslation.py
@@ -8,16 +8,6 @@ TEXT_FILE = 'text.txt'
 TARGET_FILE = 'text_translated.txt'
 
 
-def getText(file):
-    text_list = []
-    with open(file, 'r') as f:
-        while True:
-            text = f.readline()
-            if not text: break
-            text_list.append(text)
-    return text_list
-
-
 def getTextTranslated(text_list):
     text_translated_list = []
     for text in text_list:
@@ -31,37 +21,58 @@ def write(filename, content_list):
             f.write(content + '\n')
 
 
+def getfile(): #파일 여는 함수
+    global  file
+    file = askopenfilename(filetypes=[("All Files", "*.*")])
+    if file is not None:
+        lbl2.config(text=file)
+
+def changeSoundToText():
+    print("음성을 텍스트로 변환 중입니다.")
+    print(file)
+    stt = csrFunc('or41v7xmp7', 'ze8JKUSNnFleu3zmzdemIbLcKXvxlrCeNwDCBxUx', file)
+    lab4.config(text=stt)
+    print(stt)
+    text_list = []
+    text_list.append(stt)
+    text_translated_list = getTextTranslated(text_list)
+    lab6.config(text=text_translated_list)
+    print(text_translated_list)
+    progressbar.stop()
+
 if __name__ == '__main__':
 
     """2. GUI 창"""
     root = Tk()
-    root.geometry('200x100')
 
-    """3. 파일 열기"""
-    file = askopenfilename(filetypes=[("All Files", "*.*")])
-    if file is not None:
-        print(file)
+    lbl = Label(root, text = "선택한 파일 경로")
+    lbl.config(font=('바탕체', 20,'bold'))
+    lbl.grid(row=0)
 
-        file2 = file
+    lbl2 = Label(root, text='파일을 선택 해주세요.',justify= CENTER)
+    lbl2.config(font=('바탕체',15))
+    lbl2.configure(foreground = 'red')
+    lbl2.grid(row=1)
 
-    print("음성을 텍스트로 변환 중입니다.")
-    stt = csrFunc('or41v7xmp7', 'ze8JKUSNnFleu3zmzdemIbLcKXvxlrCeNwDCBxUx', file2)
+    button1 = Button(root,text='파일열기',command=getfile)
+    button1.grid(row=2)
 
-    """5. 작성한 텍스트를 파일로 저장"""
-    f = open('C:\\UnityProject\\STTWithPy\\' + TEXT_FILE, 'w')  # 원하는 폴더 경로를 설정해야 함
-    f.write(stt)
-    text_list = []
-    text_list.append(stt)
-    f.close()
-    print('변환이 완료되었습니다.')
+    button2 = Button(root, text='변환 하기', command=changeSoundToText)
+    button2.grid(row=3)
 
-    print('번역 수행중...')
-    text_translated_list = getTextTranslated(text_list)
-    write(TARGET_FILE, text_translated_list)
-    print('번역 완료\n')
+    progressbar = Progressbar(root,maximum=250,mode="indeterminate",length=250)
+    progressbar.start(2)
+    progressbar.grid(row=4)
+    lab3 = Label(root, text= '음성을 텍스트로 변환한 결과',font=('바탕체',20),justify= CENTER)
+    lab3.grid(row=6)
 
-    print('입력된 음성은 아래와 같습니다.')
-    print(stt+'\n')
+    lab4 = Label(root, text=' ', font=('바탕체', 15), justify=CENTER,foreground='red')
+    lab4.grid(row=7)
 
-    print('번역된 테스트는 아래와 같습니다')
-    print(text_translated_list)
+    lab5 = Label(root, text='텍스트를 번역한 결과', font=('바탕체', 20), anchor=CENTER,justify=CENTER)
+    lab5.grid(row=8)
+
+    lab6 = Label(root, text=' ', font=('바탕체', 15), justify=CENTER, foreground='red')
+    lab6.grid(row=9)
+
+    root.mainloop()
