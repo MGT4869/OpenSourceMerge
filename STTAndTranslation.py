d20bd7a2eebe73746e5b856bab1561d72ccb4856
MGT4869 <47241183+MGT4869@users.noreply.github.com>
2022-05-26 00:32:00 +0900

    사용하지 않는 스크립트 및 파일 정리
    - 진행바 삭제
    - 진행상태 라벨 추가

--- STTAndTranslation.py
+++ STTAndTranslation.py
@@ -3,10 +3,13 @@ from tkinter import *
 from tkinter.ttk import *
 from tkinter.filedialog import askopenfilename
 from translate import translate
+from threading import Thread
+from time import sleep
 
 TEXT_FILE = 'text.txt'
 TARGET_FILE = 'text_translated.txt'
 
+lang = 'Kor'
 
 def getTextTranslated(text_list):
     text_translated_list = []
@@ -27,18 +30,23 @@ def getfile(): #파일 여는 함수
     if file is not None:
         lbl2.config(text=file)
 
+
 def changeSoundToText():
+    lbl7.config(text='진행상태 : 음성을 텍스트로 변환 중입니다.')
     print("음성을 텍스트로 변환 중입니다.")
     print(file)
     stt = csrFunc('or41v7xmp7', 'ze8JKUSNnFleu3zmzdemIbLcKXvxlrCeNwDCBxUx', file)
     lab4.config(text=stt)
+    lbl7.config(text='진행상태 : 음성 변환 완료.')
     print(stt)
     text_list = []
     text_list.append(stt)
+    lbl7.config(text='진행상태 : 텍스트를 영어로 변환중입니다..')
     text_translated_list = getTextTranslated(text_list)
     lab6.config(text=text_translated_list)
+    lbl7.config(text='진행상태 : 변환 완료.')
     print(text_translated_list)
-    progressbar.stop()
+
 
 if __name__ == '__main__':
 
@@ -57,22 +65,26 @@ if __name__ == '__main__':
     button1 = Button(root,text='파일열기',command=getfile)
     button1.grid(row=2)
 
+    lbl7 = Label(root, text="진행상태 : 대기중",font=('바탕체',15,'bold'))
+    lbl7.grid(row=3)
+
+
+
+
+
     button2 = Button(root, text='변환 하기', command=changeSoundToText)
-    button2.grid(row=3)
+    button2.grid(row=6)
 
-    progressbar = Progressbar(root,maximum=250,mode="indeterminate",length=250)
-    progressbar.start(2)
-    progressbar.grid(row=4)
     lab3 = Label(root, text= '음성을 텍스트로 변환한 결과',font=('바탕체',20),justify= CENTER)
-    lab3.grid(row=6)
+    lab3.grid(row=7)
 
-    lab4 = Label(root, text=' ', font=('바탕체', 15), justify=CENTER,foreground='red')
-    lab4.grid(row=7)
+    lab4 = Label(root, text=' ', font=('바탕체', 15,'bold'), justify=CENTER,foreground='red')
+    lab4.grid(row=8)
 
     lab5 = Label(root, text='텍스트를 번역한 결과', font=('바탕체', 20), anchor=CENTER,justify=CENTER)
-    lab5.grid(row=8)
+    lab5.grid(row=9)
 
-    lab6 = Label(root, text=' ', font=('바탕체', 15), justify=CENTER, foreground='red')
-    lab6.grid(row=9)
+    lab6 = Label(root, text=' ', font=('바탕체', 15,'bold'), justify=CENTER, foreground='red')
+    lab6.grid(row=10)
 
     root.mainloop()
